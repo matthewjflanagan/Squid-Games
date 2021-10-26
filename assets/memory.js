c1ffb570b3b1efb265099d359649ff3fb2010893
Matthew Flanagan <mattflanagan40@gmail.com>
2021-10-26 18:30:34 -0400

    card bug fixed linked added to rps

--- assets/memory.js
+++ assets/memory.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     // console.log('clicked');
     // *this* represents the element that fired the event via event listener 
     // console.log(this);
@@ -13,31 +16,55 @@ function flipCard() {
         hasFlippedCard = true;
         firstCard = this;
 
+        return;
         // console.log({hasFlippedCard, firstCard});
-    } else {
+    } 
         // second click
         hasFlippedCard = false;
         secondCard = this;
-
         // console.log({firstCard, secondCard});
 
+    checkForMatch();
+}
+
+function checkForMatch () {
         // do cards match?
         // console.log(firstCard.dataset.framework);
         // console.log(secondCard.dataset.framework);
-        if (firstCard.dataset.framework === 
-            secondCard.dataset.framework) {
-                // its a match
-                firstCard.removeEventListener('click', flipCard);
-                secondCard.removeEventListener('click', flipCard);
-            } else {
-                // not a match
-                setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-                }, 1500);
-            } 
-    }
+        let isMatch = firstCard.dataset.framework === 
+            secondCard.dataset.framework;
+
+        isMatch ? disableCards() : unflipCards(); 
 }
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 // loop through list into each one of the cards and listen for click event and execute function flipCard
 cards.forEach(card => card.addEventListener('click', flipCard))
