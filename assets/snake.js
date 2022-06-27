2b936cbd0d180410ac8f7495422d0e25d4402972
Matthew Flanagan <mattflanagan40@gmail.com>
2022-06-26 21:55:11 -0400

    touch controls - not functional

--- assets/snake.js
+++ assets/snake.js
@@ -158,17 +158,39 @@ function checkAppleCollision() {
 
 document.body.addEventListener('keydown', keyDown);
 
+class InputHandler {
+    constructor(){
+        this.touchY = '';
+        this.touchThreshold = 30;
+
+        window.addEventListener('touchstart', e => {
+            this.touchY = e.changedTouches[0].pageY
+        });
+
+        window.addEventListener('touchmove', e => {
+            const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+            if (swipeDistance < -this.touchThreshold && this.keys.indexOf('swipe up') === -1) this.keys.push('swipe up');
+            else if (swipeDistance > this.touchThreshold && this.keys.indexOf('swipe down') === -1) this.keys.push('swipe down');
+        });
+
+        window.addEventListener('touchend', e => {
+            this.keys.splice(this.keys.indexOf('swipe up'), 1);
+            this.keys.splice(this.keys.indexOf('swipe down'), 1);
+        });
+    }
+};
+
 
 function keyDown(event) {
     //up
-    if(event.keyCode == 38 || swipedir == up) {
+    if(event.keyCode == 38) {
         if (yVelocity == 1)
             return 
         yVelocity = -1;
         xVelocity = 0;
     }
     // down
-    if(event.keyCode == 40 || swipedir == down) {
+    if(event.keyCode == 40) {
         if (yVelocity == -1)
             return 
         yVelocity = 1;
@@ -176,7 +198,7 @@ function keyDown(event) {
     }
 
     // left
-    if(event.keyCode == 37 || swipedir == left) {
+    if(event.keyCode == 37) {
         if (xVelocity == 1)
         return 
         yVelocity = 0;
@@ -184,7 +206,7 @@ function keyDown(event) {
     }
 
     // right
-    if(event.keyCode == 39 || swipedir == right) {
+    if(event.keyCode == 39) {
         if (xVelocity == -1)
         return 
         yVelocity = 0;
